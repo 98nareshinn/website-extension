372dcdd7f0345c370b1d39a28cb31b8e0fe8d6f3
Naresh SinghGehlot <aryanaresh533@gmail.com>
2022-10-20 12:08:52 -0700

    Update login.js

--- login.js
+++ login.js
@@ -4,6 +4,7 @@ var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
 if(username=="admin"&& password=="user")
 {
+    window.location.assign("website.html");
     alert("login succesfully");
     return false;
 
